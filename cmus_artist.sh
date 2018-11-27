990bbb0868f55c5bf186fd5a270ccca0753b3767
Stevolus <stevolus@protonmail.ch>
2018-11-27 09:01:26 +0200

    Update cmus_artist.sh

--- cmus_artist.sh
+++ cmus_artist.sh
@@ -2,7 +2,7 @@
 
 # Description: Output current artist and song being played in CMUS
 # Author: Steven Marchant
-# Notes: Requires cmus and cmus-remote. simply run script while cmus is playing song.
+# Notes: Requires cmus and cmus-remote. Simply run script while cmus is playing.
 
 artist=$(cmus-remote -Q | grep artist | cut -c 11-)
 title=$(cmus-remote -Q | grep title | cut -c 11-)
